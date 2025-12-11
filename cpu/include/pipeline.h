042a460efc0b2236c1d24e03793f8023d452c57f
Matthew Rockwell <matthew.h.rockwell@gmail.com>
2025-12-10 19:57:20 -0800

    Update pipeline.h
    Updated to English

--- cpu/include/pipeline.h
+++ cpu/include/pipeline.h
@@ -14,16 +14,16 @@
 namespace pipeline {
 
     using list_type = std::vector<std::pair<std::string, int> >;
-    // 从文件夹 dataset_path, 按照 categories 得到不同类别的图像列表
+    // From the folder dataset_path, get a list of images of different categories according to categories
     std::map<std::string, list_type> get_images_for_classification(
             const std::filesystem::path dataset_path,
             const std::vector<std::string> categories={},
             const std::pair<float, float> ratios={0.8, 0.1});
 
-    // 这个特别慢 !!!!, 如果有数据增强的话, 速度降低为原来的 1/4
+    // This is very slow!!!!, if there is data enhancement, the speed will be reduced to 1/4 of the original
     class ImageAugmentor {
     private:
-        std::default_random_engine e, l, c, r; // e 用来获得操作的概率; l 用来打乱操作列表; c 用来得到裁剪需要的概率; r 用来得到旋转的概率
+        std::default_random_engine e, l, c, r; // e is used to get the probability of the operation; l is used to shuffle the operation list; c is used to get the probability required for clipping; r is used to get the probability of rotation
         std::uniform_real_distribution<float> engine;
         std::uniform_real_distribution<float> crop_engine;
         std::uniform_real_distribution<float> rotate_engine;
@@ -31,32 +31,32 @@ namespace pipeline {
         std::vector<std::pair<std::string, float> > ops;
     public:
         ImageAugmentor(const std::vector<std::pair<std::string, float> >& _ops={{"hflip", 0.5}, {"vflip", 0.2}, {"crop", 0.7}, {"rotate", 0.5}})
-            : e(212), l(826), c(320), r(520),
-            engine(0.0, 1.0), crop_engine(0.0, 0.25), rotate_engine(15, 75), minus_engine(1, 10),
-            ops(std::move(_ops)) {}
+        : e(212), l(826), c(320), r(520),
+        engine(0.0, 1.0), crop_engine(0.0, 0.25), rotate_engine(15, 75), minus_engine(1, 10),
+        ops(std::move(_ops)) {}
         void make_augment(cv::Mat& origin, const bool show=false);
     };
 
     class DataLoader {
-        using batch_type = std::pair< std::vector<tensor>, std::vector<int> >; // batch 是一个 pair
+    using batch_type = std::pair< std::vector<tensor>, std::vector<int> >; // batch is a pair
     private:
-        list_type images_list; // 数据集列表, image <==> label
-        int images_num;        // 这个子数据集一共有多少张图像和对应的标签
-        const int batch_size;  // 每次打包几张图象
-        const bool augment;    // 是否要做图像增强
-        const bool shuffle;    // 是否要打乱列表
-        const int seed;        // 每次随机打乱列表的种子
-        int iter = -1;         // 当前采到了第 iter 张图像
-        std::vector<tensor> buffer; // batch 缓冲区, 用来从图像生成 tensor 的, 不用每次要读取图像时分配和销毁
-        const int H, W, C;     // 允许的图像尺寸
+        list_type images_list; // Dataset list, image <==> label
+        int images_num; // How many images and corresponding labels are there in this sub-dataset
+        const int batch_size; // Pack several images at a time
+        const bool augment; // Do you want to do image enhancement
+        const bool shuffle; // Do you want to shuffle the list
+        const int seed; // Randomly shuffle the seed of the list each time
+        int iter = -1; // The iter image is currently collected
+        std::vector<tensor> buffer; // batch buffer, used to generate tensor from image, does not need to be allocated and destroyed every time the image is to be read
+        const int H, W, C; // Allowed image size
     public:
-        explicit DataLoader(const list_type& _images_list, const int _bs=1, const bool _aug=false, const bool _shuffle=true, const std::tuple<int, int, int> image_size={224, 224, 3},  const int _seed=212);
+        explicit DataLoader(const list_type& _images_list, const int _bs=1, const bool _aug=false, const bool _shuffle=true, const std::tuple<int, int, int> image_size={224, 224, 3}, const int _seed=212);
         int length() const;
         batch_type generate_batch();
-    private:
-        // 获取第 batch_index 的图像信息, 填充成 tensor
+        private:
+        // Get the image information of the batch_index and fill it into tensor
         std::pair<tensor, int> add_to_buffer(const int batch_index);
-        // 图像增强
+        // Image enhancement
         ImageAugmentor augmentor;
     };
 }
